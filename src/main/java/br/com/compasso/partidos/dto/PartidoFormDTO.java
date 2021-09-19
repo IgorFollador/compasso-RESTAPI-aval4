b3f7b456699e32319cc6f4440728a4e97c052ef1
IgorFollador <igorledf@hotmail.com>
2021-09-19 00:52:25 -0300

    Resolvendo cadastro de Ideologia na clase Partidos

--- src/main/java/br/com/compasso/partidos/dto/PartidoFormDTO.java
+++ src/main/java/br/com/compasso/partidos/dto/PartidoFormDTO.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 import br.com.compasso.partidos.constant.Ideologia;
+import br.com.compasso.partidos.constant.Sexo;
 import lombok.Data;
 import lombok.Getter;
 
@@ -12,9 +13,13 @@ public class PartidoFormDTO {
 	
 	private String nome;
 	private String sigla;
-	private Ideologia ideologia;
+	@Getter private Ideologia ideologia;
 	@Getter private Date dataFundacao;
-	
+
+	public void setIdeologia(String ideologiaS) {
+		this.ideologia = Ideologia.valueOf(ideologiaS.toUpperCase());
+	}
+
 	public void setDataFundacao(Date dataFundacao) {
 		GregorianCalendar gc = new GregorianCalendar();
 		gc.setTime(dataFundacao);
